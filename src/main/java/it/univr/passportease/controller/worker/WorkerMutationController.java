fdc0eefcd707861aa3893b43f95ab882a25619fc
Samuel Veronese <samuel@Air-di-Samuel.homenet.telecomitalia.it>
2023-08-28 16:54:39 +0200

    fixed modifyRequest and fixed dependecies problems

--- src/main/java/it/univr/passportease/controller/worker/WorkerMutationController.java
+++ src/main/java/it/univr/passportease/controller/worker/WorkerMutationController.java
@@ -14,7 +14,6 @@ import it.univr.passportease.service.worker.WorkerMutationService;
 import lombok.AllArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 
 @Controller
