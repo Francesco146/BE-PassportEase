c4329e2975cf0fcd0a82cac7260e239a9f2ab2b2
AliceBeltrame <alicebeltrame63@gmail.com>
2023-10-12 13:22:45 +0200

    Added test for changeEmail and changePassword mutations, attempt to create test for registerUser

--- src/test/java/it/univr/passportease/graphql/GraphQLOperations.java
+++ src/test/java/it/univr/passportease/graphql/GraphQLOperations.java
@@ -1,5 +1,10 @@
 package it.univr.passportease.graphql;
 
+import it.univr.passportease.dto.input.RegisterInput;
+import jdk.jfr.Registered;
+
+import java.util.Date;
+
 public enum GraphQLOperations {
     loginUser {
         public String getGraphQl(String... args) {
@@ -27,6 +32,75 @@ public enum GraphQLOperations {
                         logout
                     }
                     """;
+        }
+    },
+
+    registerUser {
+        public String getGraphQl(String... args) {
+           String fiscalCode = args[0];
+           String email = args[1];
+           String name = args[2];
+           String surname = args[3];
+           String cityOfBirth = args[4];
+           String dateOfBirth = args[5];
+           String password = args[6];
+
+            String registerUser = """
+                    mutation {
+                        registerUser(
+                                registerInput: {
+                                    fiscalCode: "%s"
+                                    email: "%s"
+                                    name: "%s"
+                                    surname: "%s"
+                                    cityOfBirth: "%s"
+                                    dateOfBirth: "%s"
+                                    password: "%s"
+                                }
+                            ){
+                            id
+                            jwtSet {
+                                accessToken
+                                refreshToken
+                            }
+                        }
+                    }
+                    """;
+            return String.format(registerUser, fiscalCode, email, name, surname, cityOfBirth, dateOfBirth, password);
+        }
+    },
+
+    changePassword {
+        public String getGraphQl(String... args) {
+            String newPassword = args[0];
+            String oldPassword = args[1];
+
+            String changePassword = """
+                    mutation {
+                        changePassword (
+                            newPassword: "%s"
+                            oldPassword: "%s"
+                        )
+                    }
+                    """;
+            return String.format(changePassword, newPassword, oldPassword);
+        }
+    },
+
+    changeEmail {
+        public String getGraphQl(String... args) {
+            String newEmail = args[0];
+            String oldEmail = args[1];
+
+           String changeEmail = """
+                   mutation {
+                       changeEmail(
+                           newEmail: "%s"
+                           oldEmail: "%s"
+                       )
+                   }
+                   """;
+           return String.format(changeEmail, newEmail, oldEmail);
         }
     },
 
