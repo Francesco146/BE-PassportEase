0edc70836b23fdda7d8397ede72158cb1c93d409
Francesco Marastoni <marastoni14@gmail.com>
2023-10-19 17:52:23 +0200

    Fixed SLF4J duplication bug, minor bug fixes

--- src/main/java/it/univr/passportease/helper/JWT.java
+++ src/main/java/it/univr/passportease/helper/JWT.java
@@ -1,29 +1,14 @@
 package it.univr.passportease.helper;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@EqualsAndHashCode
+@ToString
 @Getter
 public class JWT {
     private static final String JWT_REGEX = "^[\\w-]*\\.[\\w-]*\\.[\\w-]*$";
     private String token;
-
-    // equals method
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof JWT jwt)) return false;
-        return token.equals(jwt.token);
-    }
-
-    // hashCode method
-    @Override
-    public int hashCode() {
-        return token.hashCode();
-    }
 }
